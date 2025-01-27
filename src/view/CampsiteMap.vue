79ec83ae50682bf90d25cec6618fecb10a425b50
Molly Kan <mollykannn@gmail.com>
2025-01-27 14:40:18 +0800

    fixed image path

--- src/view/CampsiteMap.vue
+++ src/view/CampsiteMap.vue
@@ -60,7 +60,7 @@ import { useRoute, useRouter } from "vue-router";
 import markericon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markericon from "leaflet/dist/images/marker-icon.png";
 import markershadow from "leaflet/dist/images/marker-shadow.png";
-import campsite_gemini from "@/assets/img/campsite_gemini.jpeg";
+import campsite_gemini from "../assets/img/campsite_gemini.jpeg";
 // 定義 EPSG:2326 的投影參數
 const EPSG2326 =
   "+proj=tmerc +lat_0=22.31213333333334 +lon_0=114.1785555555556 +k=1 +x_0=836694.05 +y_0=819069.8 +ellps=intl +towgs84=-162.619,-276.959,-161.764,0.067753,-2.24365,-1.15883,-1.09425 +units=m +no_defs";
